3a00047066f973c24deee62c06ff2bcb02151f1e
ravigala <72736153+ravigala@users.noreply.github.com>
2023-11-04 01:00:19 +0530

    Function to format fetched data

---
+++ src/services/dataFormatService.js
@@ -0,0 +1,32 @@
+async function formatData(response) {
+    try {
+      let stockSymbols = {};
+  
+      for (let i = 0; i < response.length; i++) {
+        let stock = response[i];
+        let stockName = 'NSE:' + stock[2].replace('-', '_');
+        let groupNumber = Math.ceil((i + 1) / 30);
+        let groupName = 'group' + groupNumber;
+  
+        if (!stockSymbols[groupName]) {
+          stockSymbols[groupName] = [];
+        }
+  
+        stockSymbols[groupName].push(stockName);
+      }
+  
+      console.log(stockSymbols);
+  
+      const formattedResponse = {
+        status: "success",
+        msg: stockSymbols
+      };
+  
+      return formattedResponse;
+  
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  }
+
+module.exports = { formatData };
